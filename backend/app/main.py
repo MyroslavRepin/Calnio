7a462ad114cd19cb7e5d99ddd2feaf901138bd99
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-05 20:16:43 -0600

    Refractoring code

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,18 @@
-import os
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from app.api import login, signup, landing, dashboard
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "frontend"))
 
-templates = Jinja2Templates(directory=os.path.join(FRONTEND_DIR, "templates"))
+templates = Jinja2Templates(directory=FRONTEND_DIR)
 
-app.mount("/static", StaticFiles(directory=os.path.join(FRONTEND_DIR,
-          "static")), name="static")
+app.mount("/static", StaticFiles(directory=FRONTEND_DIR), name="static")
 
-# Connecting Routes
 app.include_router(login.router)
 app.include_router(signup.router)
 app.include_router(landing.router)
