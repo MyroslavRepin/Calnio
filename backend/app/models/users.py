888e0fa6df6fda1d0342e50bf4b1dc29f8024651
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-07 13:45:35 -0600

    Adding model User, left to do: add session and connecting to db

---
+++ backend/app/models/users.py
@@ -0,0 +1,14 @@
+from db.base_class import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    admin = Column(Boolean, default=False)
