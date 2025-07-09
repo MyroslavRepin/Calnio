3960c5be694ea26da0094a216546efdf92a28e52
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-09 16:25:17 -0600

    Refactor User model to use mapped_column and update user creation command to set is_superuser

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,15 +1,20 @@
 from backend.app.db.database import Base
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
+from typing import Optional
 
 
 class User(Base):
     __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    is_superuser = Column(Boolean, default=False)
-    is_active = Column(Boolean, default=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[Optional[str]] = mapped_column(nullable=True)
+    # id = Column(Integer, primary_key=True)
+    # email = Column(String, unique=True, index=True)
+    # username = Column(String, unique=True, index=True)
+    # hashed_password = Column(String, nullable=False)
+    # created_at = Column(DateTime, default=datetime.utcnow)
+    # is_superuser = Column(Boolean, default=False)
+    # is_active = Column(Boolean, default=True)
