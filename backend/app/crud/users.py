3ffa4d1f0789bc663b40e94ff4bee9451af5812a
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-09 15:04:13 -0600

    Adding simple testing functions and cleaning structure

--- backend/app/crud/users.py
+++ backend/app/crud/users.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from backend.app.models.users import User
 from backend.app.schemas.users import UserCreate
 from hashlib import sha256
@@ -9,13 +10,18 @@ def get_users(db: Session):
 
 
 def create_user(db: Session, user: UserCreate):
-    hashed_password = sha256(user.password.encode()).hexdigest()
-    db_user = User(
-        email=user.email,
-        username=user.username,
-        hashed_password=hashed_password
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    try:
+        hashed_password = sha256(user.password.encode()).hexdigest()
+        db_user = User(
+            email=user.email,
+            username=user.username,
+            hashed_password=hashed_password
+        )
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except IntegrityError:
+        db.rollback()
+        raise ValueError(
+            "❌ Пользователь с таким email или username уже существует")
