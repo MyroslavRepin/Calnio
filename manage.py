28701dea3cdabc26717578abbd2fa61c717299c2
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-09 22:44:29 -0600

    Enhance user management features: add user table printing, update user model with timestamps and superuser flag, and modify user creation schema. Update requirements file.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from backend.app.db.check_db import check_connection
 from backend.app.db.create_tables import create_tables
 from backend.app.db.database import SessionLocal
 from backend.app.schemas.users import UserCreate
-from backend.app.crud.users import create_user, get_users
+from backend.app.crud.users import create_user, get_users, print_users_table
 from backend.app.db.database import DATABASE_URL
 
 
@@ -19,12 +19,21 @@ def main():
     elif command == "create":
         create_tables()
     elif command == "get_users":
-        get_users(SessionLocal())
+        db = SessionLocal()
+        try:
+            users = get_users(db)
+            print_users_table(users)
+        finally:
+            db.close()
+
     elif command == 'create_user':
         try:
-            user = UserCreate(username="testadmin",
+            user = UserCreate(username="testuser",
                               email="test@example.com", password="password123", is_superuser=True)
-            create_user(db=SessionLocal(), user=user)
+            user2 = UserCreate(username="jessica",
+                               email="jesica@example.com", password="mikhaiel", is_superuser=False)
+            # create_user(db=SessionLocal(), user=user)
+            create_user(db=SessionLocal(), user=user2)
             print("✅ User created successfully.")
         finally:
             SessionLocal().close()
