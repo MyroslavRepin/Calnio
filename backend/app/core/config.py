d6f672a5c77a6b1fac2d8d58d2256166fd5946c9
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-10 11:09:26 -0600

    Add database configuration to .env file and clean up config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,7 @@
+import os
+
+# DATABASE_URL_LOCAL = "postgresql://myroslav:myroslav0818@localhost:5432/scheduloo"
+# DATABASE_URL_PROD = "postgresql://postgres:TxgyMUDOCAXOedvxSoXVsteYpvuprNnt@turntable.proxy.rlwy.net:25860/railway"  # RAILWAY
+
+
 DATABASE_URL = "postgresql://myroslav:myroslav0818@localhost:5432/scheduloo"
