ee13fc4e09664558ed2ac6f866ab12e074850fb8
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-14 22:25:49 -0600

    Refactor user model and schema: remove unused imports and update password length requirement

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class UserCreate(BaseModel):
-    email: EmailStr  # автоматическая проверка email
+    email: EmailStr
     username: str = Field(..., min_length=3, max_length=30)
-    password: str = Field(..., min_length=6)
+    password: str = Field(..., min_length=8)
     is_superuser: bool = False
