3aceb14fb31b3fa4b9a550dab9e0b21ecb68dee2
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-16 11:12:14 -0600

    Adding simple logics and can generate JWT token

--- backend/app/api/login.py
+++ backend/app/api/login.py
@@ -1,10 +1,24 @@
 import os
-from fastapi import APIRouter, Request, Form
+from authx import AuthX, AuthXConfig
+
+from fastapi import APIRouter, Request, Form, Depends, HTTPException, Response
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 
+from backend.app.schemas.users import UserLogin
+
 router = APIRouter()
-# Adding externaly templates, static dir
+
+
+#! JWT Token CONFIG
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "secret_key"
+config.JWT_ACCESS_COOKIE_NAME = "acces_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=config)
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_DIR = os.path.abspath(os.path.join(
     BASE_DIR, "..", "..", "..", "frontend"))
@@ -18,8 +32,10 @@ async def login(request: Request):
 
 
 @router.post('/login')
-async def login_post(request: Request, email: str = Form(...), password: str = Form(...)):
-    form = await request.form()
-    print(f'Data from form: {form}')
-    print(f"Email: {email}, password: {password}")
-    return RedirectResponse('/', 303)
+async def login_post(response: Response, creds: UserLogin = Depends(UserLogin.as_form)):
+    if creds.email == "myroslavrepin@gmail.com" and creds.password == "myroslav0818":
+        token = security.create_access_token(uid="12456")
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        print(token)
+        return {"acces_token": token}
+    raise HTTPException(401, detail='Incorrect data')
--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel, EmailStr
+from fastapi import Form
 from pydantic import BaseModel, Field, EmailStr
 
 
@@ -6,3 +8,19 @@ class UserCreate(BaseModel):
     username: str = Field(..., min_length=3, max_length=30)
     password: str = Field(..., min_length=8)
     is_superuser: bool = False
+
+
+# backend/app/schemas/users.py
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+    @classmethod
+    def as_form(
+        cls,
+        email: str = Form(...),
+        password: str = Form(...)
+    ):
+        return cls(email=email, password=password)
