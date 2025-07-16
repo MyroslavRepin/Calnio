02aa27e179a2541bfb3f5fd25bff2bbc6aa92f65
MyroslavRepin <myroslavrepin@gmail.com>
2025-07-16 15:00:08 -0600

    Adding login page a JWT tokens and sign up, and much more

--- backend/app/api/dashboard.py
+++ backend/app/api/dashboard.py
@@ -1,12 +1,15 @@
 import os
 
 from backend.app.db.database import AsyncSessionLocal
+from backend.app.security.jwt_tokens import security
+from backend.app.security.utils import access_token_required
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi import APIRouter, Request, Form
+from fastapi import APIRouter, Request, Form, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.exceptions import HTTPException
 
 router = APIRouter()
 
@@ -19,4 +22,9 @@ templates = Jinja2Templates(directory=os.path.join(FRONTEND_DIR, "templates"))
 
 @router.get("/dashboard", response_class=HTMLResponse)
 async def dashboard(request: Request):
+    try:
+        payload = await access_token_required(request)
+    except HTTPException:
+        # ❌ Если токен невалиден или отсутствует — редирект или HTML
+        return templates.TemplateResponse("unauthorized.html", {"request": request}, status_code=401)
     return templates.TemplateResponse("dashboard.html", {"request": request})
