c96c43222e0ee5aa655f1ec4b828c4945523ecb6
Myroslav Repin <myroslavrepin@gmail.com>
2025-09-04 19:03:30 -0600

    Add error 404 handling and update landing page links for better navigation. NON-PROD

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from backend.app.api import login, signup, landing, dashboard, users, logout, refresh
+from backend.app.api import login, signup, landing, dashboard, users, logout, refresh, error_404
 from backend.app.api.oauth import notion_callback
 from backend.app import version
 from backend.app.middleware.ignore_logging import IgnoreSpecificPathsMiddleware
@@ -31,6 +31,7 @@ app.include_router(users.router)
 app.include_router(logout.router)
 app.include_router(refresh.router)
 app.include_router(notion_callback.router)
+app.include_router(error_404.router)
 
 
 class UserAdmin(ModelView, model=user_models.User):
