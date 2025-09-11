8ef985fca28fdfe7f21172f286ac47adbd548a57
Myroslav Repin <myroslavrepin@gmail.com>
2025-09-10 18:18:43 -0600

    Adding controling environments from the .env, updating .env structure

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,28 +8,80 @@ load_dotenv(dotenv_path, override=True)
 
 
 class Settings(BaseSettings):
-    env: str = "local"
-    database_url_local: str
+    env: str
+    db: str
+    https: bool
+    notion_send_data_env: str
+
     database_url_prod: str
-    oauth_client_id: str
-    oauth_client_secret: str
-    notion_redirect_uri: str
+    database_url_local: str
+
+    notion_oauth_client_id_prod: str
+    notion_oauth_client_id_local: str
+
+    notion_oauth_secret_prod: str
+    notion_oauth_secret_local: str
+
+    notion_redirect_prod: str
+    notion_redirect_local: str
+    notion_redirect_local_https: str
+
+    oauth_url_prod: str
+    oauth_url_local: str
 
     @property
-    def database_url(self) -> str:
-        return self.database_url_prod if self.env == "prod" else self.database_url_local
+    def database_url(self):
+        if self.env == "prod":
+            if self.db == "local":
+                return self.database_url_local
+        else:
+            return self.database_url_local
 
     @property
-    def redirect_uri(self) -> str:
-        return (
-            "https://calnio.com/oauth/callback"
-            if self.env == "prod"
-            else "http://localhost:8000/oauth/callback"
-        )
+    def notion_redirect_uri(self):
+        if self.env == "prod":
+            if self.notion_send_data_env == "prod":
+                return self.notion_redirect_prod
+            else:
+                return self.notion_redirect_local
+
+        else:
+            if self.https == "True":
+                if self.notion_send_data_env == "prod":
+                    return self.notion_redirect_prod
+                else:
+                    return self.notion_redirect_local_https
+            else:
+                if self.notion_send_data_env == "prod":
+                    return self.notion_redirect_prod
+                else:
+                    return self.notion_redirect_local
+
+    @property
+    def notion_secert(self):
+        if self.env == "prod":
+            return self.notion_oauth_secret_prod
+        else:
+            return self.notion_oauth_secret_local
+
+    @property
+    def notion_client_id(self):
+        if self.env == "prod":
+            return self.notion_oauth_client_id_prod
+        else:
+            return self.notion_oauth_client_id_local
+
+    @property
+    def notion_oauth_url(self):
+        if self.env == "prod":
+            return self.oauth_url_prod
+        else:
+            return self.oauth_url_local
 
     class Config:
         env_file = str(Path(__file__).resolve(
         ).parents[3] / ".env")  # путь к .env
+        env_file_encoding = "utf-8"
 
 
-settings = Settings()
+settings = Settings()  # pyright: ignore[reportCallIssue]
