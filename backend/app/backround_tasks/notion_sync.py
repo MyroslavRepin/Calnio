a7d5f98f5df6f6901abd10f15964f86639495ef5
Myroslav Repin <myroslavrepin@gmail.com>
2025-09-25 12:13:25 -0600

    Refactor Notion integration: unify background task execution into `notion_sync_background`, streamline task handling in API endpoint

--- backend/app/backround_tasks/notion_sync.py
+++ backend/app/backround_tasks/notion_sync.py
@@ -223,3 +223,14 @@ async def update_pages_by_ids(db: AsyncSession, notion: AsyncClient, user_id: in
         }
         await update_task(db=db, user_id=user_id, data=data, task=task)
         logging.debug(f"Updated page_id: {page_id}")
+
+async def notion_sync_background(db, notion, user_id):
+    added = await add_tasks_to_db(db, notion, user_id)
+    current_notion_pages = await get_all_ids(notion)
+    deleted = await delete_pages_by_ids(db, notion, user_id, current_notion_pages)
+    updated = await update_pages_by_ids(db, notion, user_id, current_notion_pages)
+    return {
+        "added": added,
+        "deleted": deleted,
+        "updated": updated
+    }
