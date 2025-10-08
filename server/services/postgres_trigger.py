b4b71935120a2ffe537015df40f6fccca01a6cf5
Myroslav Repin <myroslavrepin@gmail.com>
2025-10-08 16:17:40 -0600

    Enhance Notion integration: update task serialization and sync logic, improve webhook handling, and refactor database interactions Saving part of not finished work

--- server/services/postgres_trigger.py
+++ server/services/postgres_trigger.py
@@ -4,9 +4,14 @@ import json
 import asyncpg
 
 from server.db.database import engine
-import select
+from sqlalchemy import select
 from server.app.core.logging_config import logger
 from server.app.core.config import settings
+from server.db.deps import async_get_db_cm
+from server.db.models import User, UserNotionTask
+from server.db.redis_client import get_redis
+from server.utils.redis.utils import save_webhook_data
+
 
 async def listen_to_postgres(channel='my_channel'):
     conn = await asyncpg.connect("postgresql://postgres:TxgyMUDOCAXOedvxSoXVsteYpvuprNnt@turntable.proxy.rlwy.net:25860/railway")
@@ -16,10 +21,20 @@ async def listen_to_postgres(channel='my_channel'):
         await asyncio.sleep(3600)  # держим соединение открытым
 
 
-def handle_notification(conn, pid, channel, payload):
+async def handle_notification(conn, pid, channel, payload):
     data = json.loads(payload)
-    operation = data.get("operation")
-    task_id = data.get("id")
+    page_id = data.get("id")
 
-    # компактно, в одну строку
-    logger.debug(f"Operation={operation}, id={task_id}")
+    async with async_get_db_cm() as db:
+        stmt = select(UserNotionTask.user_id).where(UserNotionTask.id == page_id)
+        result = await db.execute(stmt)
+        user_id = result.scalars().first()
+    operation = data.get("operation")
+    data = {
+        "page_id": page_id,
+        "operation": operation,
+        "user_id": user_id,
+    }
+    redis_client = await get_redis()
+    await save_webhook_data(user_id, data, redis=redis_client)
+    logger.debug(f"Recieved Postgres notification payload: {data}")
