f9855abacb5cee28acab498cdd58c216c959d937
Myroslav Repin <myroslavrepin@gmail.com>
2025-11-14 01:08:10 -0700

    Add file logging via Loguru, update logging configuration, and map logs directory in `docker-compose.yml`

--- server/app/core/logging_config.py
+++ server/app/core/logging_config.py
@@ -1,6 +1,10 @@
+import os
+
 from loguru import logger
 import sys
 import logging
+
+os.makedirs("/calnio/logs", exist_ok=True)
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("httpcore").setLevel(logging.WARNING)
@@ -10,6 +14,18 @@ logger.remove()
 logger.add(sys.stdout, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
            backtrace=False, diagnose=False, colorize=True)
 
+# Saving logs to file
+os.makedirs("/calnio/logs", exist_ok=True)
+
+# File - full traceback
+logger.add("/calnio/logs/app_{time:YYYY-MM-DD}.log",
+           rotation="1 day",
+           retention="14 days",
+           compression="zip",
+           format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {name}:{function}:{line} - {message}",
+           level="DEBUG",
+           backtrace=True,
+           diagnose=True)
 
 # Optionally, set log level here
 logger.level("INFO")
