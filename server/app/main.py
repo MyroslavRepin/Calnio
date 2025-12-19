29b99bc781a7e6aeba217872fc701b162a8daff5
Myroslav Repin <myroslavrepin@gmail.com>
2025-12-19 15:59:22 -0700

    refactor: Deleted unused directories, fixed path for logs saving

--- server/app/main.py
+++ server/app/main.py
@@ -50,7 +50,7 @@ logger.add(
 )
 
 os.makedirs("logs", exist_ok=True)
-logger.add("logs/app_{time:YYYY-MM-DD}.log",
+logger.add("logs/server/app_{time:YYYY-MM-DD}.log",
            rotation="1 day",
            retention="14 days",
            compression="zip",
